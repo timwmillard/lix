0c51047013445d97ed7f59634ffec873b66bf243
Tim Millard <tim@rapidpaylegal.com>
2023-05-31 10:15:23 +1000

    Update lue example

--- examples/demo.lua
+++ examples/demo.lua
@@ -6,6 +6,11 @@ lix.database = {
     password = "password"
 }
 
+lix.cache = {
+    client = "redis",
+    port = 6000
+}
+
 lix.http.defaultContentType = "application/json"
 
 lix.routes = {
@@ -35,8 +40,19 @@ function users(req, body, res)
     userId = req.var["user_id"]
     user = lix.sqlquery(homesql, userId)
     return {
-        name = user.Name
+        name = user.Name,
         age = toAge(user.DOB)
     }
 end
 
+function payment(req, res)
+   tx = lix.begin_tx()
+  
+    result = tx.query_file("payment.sql", req.payment_id)
+
+    stripe.customer.create({
+        name = result.first_name
+    })
+
+   tx.commit()
+end
